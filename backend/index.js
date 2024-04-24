7c241ce95d7ae1d3236ccbbaa14b52ff48d0a9e5
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-04-24 23:48:45 +0500

    Mongo DB connection established

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const dbConnect = require("./config/dbConnect");
 require("dotenv").config();
 const app = express();
 
+dbConnect();
 
 app.listen(process.env.PORT,()=>{
   console.log(`Server listening at port ${process.env.PORT}`);
