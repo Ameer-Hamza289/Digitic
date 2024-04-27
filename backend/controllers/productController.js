ea67f86eab5abccee6a934f469e8239d891a064c
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-04-27 23:53:58 +0500

    Product Controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,22 @@
+const Product = require("../models/productModel");
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
+const validateMongoDbId = require("../utils/validateMongodbId");
+
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+
+module.exports={
+  createProduct
+}
