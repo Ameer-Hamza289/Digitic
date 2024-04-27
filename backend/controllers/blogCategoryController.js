e089453172e2e2565d0788f4e4496ccaa3fa7861
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-04-27 23:52:42 +0500

    Blog Category Controller

---
+++ backend/controllers/blogCategoryController.js
@@ -0,0 +1,17 @@
+const Category = require("../models/blogCatModel");
+const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbId");
+
+const createCategory = asyncHandler(async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.json(newCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+
+module.exports={
+  createCategory
+}
