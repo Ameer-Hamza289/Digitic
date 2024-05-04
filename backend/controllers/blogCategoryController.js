3a3100b5f01b8faf977a28c2c303bc929d17ac90
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-05-04 16:38:00 +0500

    Blog Category controllers

--- backend/controllers/blogCategoryController.js
+++ backend/controllers/blogCategoryController.js
@@ -11,7 +11,52 @@ const createCategory = asyncHandler(async (req, res) => {
   }
 });
 
+const updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(id);
+    res.json(deletedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const getCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getaCategory = await Category.findById(id);
+    res.json(getaCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const getallCategory = asyncHandler(async (req, res) => {
+  try {
+    const getallCategory = await Category.find();
+    res.json(getallCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 
 module.exports={
-  createCategory
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategory,
+  getallCategory,
 }
