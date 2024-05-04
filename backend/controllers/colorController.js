1575e3f7f25551bd54bd0b08f5deb9eab074c8f8
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-05-04 16:46:20 +0500

    Color controllers added

--- backend/controllers/colorController.js
+++ backend/controllers/colorController.js
@@ -2,10 +2,52 @@ const Coupon = require("../models/couponModel");
 const validateMongoDbId = require("../utils/validateMongodbId");
 const asynHandler = require("express-async-handler");
 
-const createCoupon = asynHandler(async (req, res) => {
+
+const createColor = asyncHandler(async (req, res) => {
   try {
-    const newCoupon = await Coupon.create(req.body);
-    res.json(newCoupon);
+    const newColor = await Color.create(req.body);
+    res.json(newColor);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updateColor = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const updatedColor = await Color.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedColor);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const deleteColor = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deletedColor = await Color.findByIdAndDelete(id);
+    res.json(deletedColor);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const getColor = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getaColor = await Color.findById(id);
+    res.json(getaColor);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const getallColor = asyncHandler(async (req, res) => {
+  try {
+    const getallColor = await Color.find();
+    res.json(getallColor);
   } catch (error) {
     throw new Error(error);
   }
@@ -13,5 +55,9 @@ const createCoupon = asynHandler(async (req, res) => {
 
 
 module.exports={
-  createCoupon
+  createColor,
+  updateColor,
+  deleteColor,
+  getColor,
+  getallColor
 }
