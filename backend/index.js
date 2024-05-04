86dff94c5dbd5186fd9a3c58756d5c3c07931d21
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-05-04 17:40:49 +0500

    APIs developed

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,16 @@ require("dotenv").config();
 const app = express();
 const cors=require("cors");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
-// const authRouter = require("./routes/authRoute");
-// const productRouter = require("./routes/productRoute");
-// const blogRouter = require("./routes/blogRoute");
-// const categoryRouter = require("./routes/prodcategoryRoute");
-// const blogcategoryRouter = require("./routes/blogCatRoute");
-// const brandRouter = require("./routes/brandRoute");
-// const colorRouter = require("./routes/colorRoute");
-// const enqRouter = require("./routes/enqRoute");
-// const couponRouter = require("./routes/couponRoute");
-// const uploadRouter = require("./routes/uploadRoute");
+const authRouter = require("./routes/authRoutes");
+const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
+const categoryRouter = require("./routes/prodcategoryRoute");
+const blogcategoryRouter = require("./routes/blogCatRoute");
+const brandRouter = require("./routes/brandRoute");
+const colorRouter = require("./routes/colorRoute");
+const enqRouter = require("./routes/enqRoute");
+const couponRouter = require("./routes/couponRoute");
+const uploadRouter = require("./routes/uploadRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 
@@ -25,16 +25,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use("/api/user", authRouter);
-// app.use("/api/product", productRouter);
-// app.use("/api/blog", blogRouter);
-// app.use("/api/category", categoryRouter);
-// app.use("/api/blogcategory", blogcategoryRouter);
-// app.use("/api/brand", brandRouter);
-// app.use("/api/coupon", couponRouter);
-// app.use("/api/color", colorRouter);
-// app.use("/api/enquiry", enqRouter);
-// app.use("/api/upload", uploadRouter);
+app.use("/api/user", authRouter);
+app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/blogcategory", blogcategoryRouter);
+app.use("/api/brand", brandRouter);
+app.use("/api/coupon", couponRouter);
+app.use("/api/color", colorRouter);
+app.use("/api/enquiry", enqRouter);
+app.use("/api/upload", uploadRouter);
 
 
 
