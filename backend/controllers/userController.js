df65dd28b9331795322a8b05d3f3e19ebb8d5317
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-05-05 23:15:48 +0500

    Spelling Correction

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,7 +66,7 @@ const loginAdmin = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   // check if user exists or not
   const findAdmin = await User.findOne({ email });
-  if (findAdmin.role !== "admin") throw new Error("Not Authorised");
+  if (findAdmin.role !== "admin") throw new Error("Not Authorized");
   if (findAdmin && (await findAdmin.isPasswordMatched(password))) {
     const refreshToken = await generateRefreshToken(findAdmin?._id);
     const updateuser = await User.findByIdAndUpdate(
